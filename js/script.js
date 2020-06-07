0e037113e3a5045f32b6a3c4f15dc4abf6d13693
MrTetris-rv <sharailee@bk.ru>
2020-06-07 18:54:54 +0300

    for mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-home = document.getElementById("home");
+//button Menu
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
